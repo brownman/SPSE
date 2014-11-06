c09202633a492e625f2adf5a6aa0cefe1b505d2a
brownman <brownman556@gmail.com>
2014-11-06 15:07:23 +0200

    Create modules.py

---
+++ BANK/PYTHON/BANK/7/modules.py
@@ -0,0 +1,13 @@
+Modules
+----
+- load other files's code by using keywords: import+from
+```python
+#import a class
+from Module1 import Class2
+
+#create an instance:
+instance1 = Module1.Class2.func1('my name', 'my family name')
+
+#use object's method:
+instance1.show()
+```
